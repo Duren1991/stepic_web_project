dadb0afe9952b28f13fa83f59b6a094ce3d4b0c3
Duren1991 <duren_91@mail.ru>
2016-11-17 00:52:14 +0300

    2.1.5

--- etc/django.py
+++ etc/django.py
@@ -1,2 +1,10 @@
-pythonpath = '/home/box/web/ask/ask'
-bind = "0.0.0.0:8000"
+CONFIG = {
+    'mode': 'wsgi',
+    'working_dir': '/home/box/web/ask',
+    'args': (
+        '--bind=0.0.0.0:8000',
+        '--workers=4',
+        '--timeout=60',
+        'ask.wsgi:application'
+    )
+}
--- init.sh
+++ init.sh
@@ -2,7 +2,7 @@
 #!/usr/bin/env bash
 
 sudo ln -fs /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
-/etc/init.d/nginx restart
+sudo /etc/init.d/nginx restart
 
 sudo gunicorn -c /home/box/web/etc/hello.py hello:app --daemon
 sudo gunicorn -c /home/box/web/etc/django.py wsgi --daemon
