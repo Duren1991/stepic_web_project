e0a3e6ce4cafe5975b9e7bfbc80e4dbc4139bba7
Duren1991 <duren_91@mail.ru>
2016-11-17 11:37:20 +0300

    2.1.7

--- hello.py
+++ hello.py
@@ -1,19 +1,11 @@
 def app(environ, start_response):
-
-	
-	raw_uri = str(environ.get('RAW_URI'))
-	
-	raw_uri = raw_uri[2:]
-
-	params = raw_uri.split('&')
-	
-	data = ''
-	for param in params:
-		data += param + '\r\n'
-	
-	start_response("200 OK", [
-	  ("Content-Type", "text/plain"),
-	  ("Content-Length", str(len(data)))
-	])
-	return iter([data])
-
+    """Simplest possible application object"""
+    data = '\n'.join(environ['QUERY_STRING'].split('&'))
+    #data = 'Hello, World!\n'
+    status = '200 OK'
+    response_headers = [
+        ('Content-type','text/plain'),
+        ('Content-Length', str(len(data)))
+    ]
+    start_response(status, response_headers)
+    return iter([data])
--- init.sh
+++ init.sh
@@ -1,8 +1,11 @@
-
-#!/usr/bin/env bash
-
-sudo ln -fs /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
+sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
 
-sudo gunicorn -c /home/box/web/etc/hello.py hello:app --daemon
-sudo gunicorn -c /home/box/web/etc/django.py wsgi --daemon
+sudo ln -sf /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test
+sudo ln -sf /home/box/web/etc/gunicorn_ask.conf /etc/gunicorn.d/ask
+sudo /etc/init.d/gunicorn restart
+
+sudo /etc/init.d/mysql start
+mysql -uroot -e "create database qa"
+#gunicorn -b 0.0.0.0:8080 hello:app
+#./script.sh
