0a7bfbc81d5ae7b3df03a0f74e72d469f179bdbf
Duren1991 <duren_91@mail.ru>
2016-11-18 13:21:46 +0300

    2.3.5

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -4,33 +4,33 @@ from django.contrib.auth.models import User
 
 
 # Create your models here.
+
+
+class Question(models.Model):
+    objects = QuestionManager() 
+    title = models.CharField(max_length=255)
+    text = models.TextField()
+    added_at = models.DateField(blank=True,auto_now_add=True)
+    rating = models.IntegerField(default=0)
+    author = models.ForeignKey(User,default=1)
+    likes = models.ManyToManyField(User, related_name='question', blank=True)
+    #likes = models.TextField()
 class QuestionManager(models.Manager):                                          
     def new(self):                                                              
 	return self.order_by('added_at')
                                                           
     def popular():                                                          
         return self.order_by('rating') 
-
-class Question(models.Model):
-    objects = QuestionManager() 
-    title = models.CharField(max_length=255)
-    text = models.TextField()
-    added_at = models.DateField(auto_now_add=True)
-    rating = models.IntegerField(default=0)
-    author = models.ForeignKey(User,default=1)
-    #likes = models.ManyToManyField(User, related_name='questions', blank=True)
-    likes = models.TextField()
-
-    def __unicode__(self):
+    '''def __unicode__(self):
         return self.title
 
     def get_absolute_url(self):
-        return reverse('question', kwargs={"id": self.id})
+        return reverse('question', kwargs={"id": self.id})'''
 
 
 
 class Answer(models.Model):
     text = models.TextField()
-    added_at = models.DateField(auto_now_add=True)
+    added_at = models.DateField(blank=True,auto_now_add=True)
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     author = models.ForeignKey(User, default=1)
